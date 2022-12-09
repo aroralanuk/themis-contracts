1b035d5ea93086084b38d51ee9efac96b42fac96
Kunal Arora <kuarora@ucsd.edu>
2022-12-08 23:42:02 -0800

    themisController reveal

--- src/ThemisController.sol
+++ src/ThemisController.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.15;
 
 import {Auction} from "src/lib/Auction.sol";
+import {LibBalanceProof} from "src/lib/LibBalanceProof.sol";
 
 import {IThemis} from "src/IThemis.sol";
 import {ThemisVault} from "src/ThemisVault.sol";
@@ -16,9 +17,26 @@ contract ThemisController is IThemis {
     uint96 revealStartBlock;
     bytes32 storedBlockHash;
 
+    bool isCollateralized;
 
-    constructor(uint32 domain_, address contract_) {
-        // query from
+    /// @dev A Merkle proof and block header, in conjunction with the
+    ///      stored `collateralizationDeadlineBlockHash` for an auction,
+    ///      is used to prove that a bidder's `SneakyVault` was sufficiently
+    ///      collateralized by the time the first bid was revealed.
+    /// @param accountMerkleProof The Merkle proof of a particular account's
+    ///        state, as returned by the `eth_getProof` RPC method.
+    /// @param blockHeaderRLP The RLP-encoded header of the block
+    ///        for which the account balance is being proven.
+    struct CollateralizationProof {
+        bytes[] accountMerkleProof;
+        bytes blockHeaderRLP;
+    }
+
+
+    constructor() {
+    }
+
+    function connectAuction(uint32 domain_, address contract_) external {
         auction = Auction.format(domain_, contract_);
     }
 
@@ -34,10 +52,11 @@ contract ThemisController is IThemis {
         emit RevealStarted();
     }
 
-    function reveal(
+    function revealBid(
         address bidder_,
         uint128 bidAmount_,
-        bytes32 salt_
+        bytes32 salt_,
+        CollateralizationProof calldata proof_
     ) external {
         address vault = getVaultAddress(
             auction,
@@ -49,6 +68,24 @@ contract ThemisController is IThemis {
         if (revealedVault[vault]) revert BidAlreadyRevealed();
         revealedVault[vault] = true;
 
+        uint256 vaultBalance = _getProvenAccountBalance(
+            proof_.accountMerkleProof,
+            proof_.blockHeaderRLP,
+            storedBlockHash,
+            vault
+        );
+        if (vaultBalance < bidAmount_) {
+            new ThemisVault{salt: salt_}(
+                auction,
+                bidder_,
+                bidAmount_
+            );
+            isCollateralized = false;
+        }
+
+        if (isCollateralized) {
+            // _dispatch();
+        }
 
     }
 
@@ -74,7 +111,36 @@ contract ThemisController is IThemis {
         )))));
     }
 
-    function _getProvenVaultBalance() external {}
+    /// @dev Gets the balance of the given account at a past block by
+    ///      traversing the given Merkle proof for the state trie. Wraps
+    ///      LibBalanceProof.getProvenAccountBalance so that this function
+    ///      can be overriden for testing.
+    /// @param proof A Merkle proof for the given account's balance in
+    ///        the state trie of a past block.
+    /// @param blockHeaderRLP The RLP-encoded block header for the past
+    ///        block for which the balance is being queried.
+    /// @param blockHash The expected blockhash. Should be equal to the
+    ///        Keccak256 hash of `blockHeaderRLP`.
+    /// @param account The account whose past balance is being queried.
+    /// @return accountBalance The proven past balance of the account.
+    function _getProvenAccountBalance(
+        bytes[] memory proof,
+        bytes memory blockHeaderRLP,
+        bytes32 blockHash,
+        address account
+    )
+        internal
+        virtual
+        view
+        returns (uint256 accountBalance)
+    {
+        return LibBalanceProof.getProvenAccountBalance(
+            proof,
+            blockHeaderRLP,
+            blockHash,
+            account
+        );
+    }
 
     function max(uint256 _a, uint256 _b) internal pure returns (uint256) {
         return _a > _b ? _a : _b;
--- src/ThemisVault.sol
+++ src/ThemisVault.sol
@@ -1,6 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.15;
 
+import "solmate/utils/SafeTransferLib.sol";
+import {ThemisController} from "./ThemisController.sol";
 
+/// @title A contract deployed via `CREATE2` by the `ThemisController` contract.
+/// Bidders send their collateral to the address of the SneakyVault before it is deployed.
 contract ThemisVault {
+    using SafeTransferLib for address;
+
+    constructor(
+        bytes32 auction,
+        address bidder,
+        uint128 /* bidValue */
+    ) {
+        // This contract should be deployed via `CREATE2` by a `ThemisController` contract
+        ThemisController controller = ThemisController(msg.sender);
+        // If this vault holds the collateral for the winning bid, send the bid amount
+        // to the seller
+
+        // TODO: fix this
+        // if (controller.getHighestBidVault(auction, tokenId) == address(this)) {
+        //     uint256 bidAmount = controller.getSecondHighestBid(tokenContract, tokenId);
+        //     assert(address(this).balance >= bidAmount);
+        //     controller.getSeller(tokenContract, tokenId).safeTransferETH(bidAmount);
+        // }
+
+        // Self-destruct, returning excess ETH to the bidder
+        selfdestruct(payable(bidder));
+    }
 }
