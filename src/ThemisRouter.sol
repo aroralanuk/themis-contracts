d306fa1e1b5c82cd80a4e7814c0597264dc9e29f
Kunal Arora <kuarora@ucsd.edu>
2022-12-13 16:54:02 -0800

    Liquidity routing

--- src/ThemisRouter.sol
+++ src/ThemisRouter.sol
@@ -1,15 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.15;
 
-import {Router} from "@hyperlane-xyz/core/contracts/Router.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-contract ThemisRouter is Router  {
+import {TypeCasts} from "@hyperlane-xyz/core/contracts/libs/TypeCasts.sol";
+import {Router} from "@hyperlane-xyz/core/contracts/Router.sol";
+import {ILiquidityLayerRouter} from "./interfaces/ILiquidityLayerRouter.sol";
+import {ILiquidityLayerAdapter} from "./interfaces/ILiquidityLayerAdapter.sol";
+import {ILiquidityLayerMessageRecipient} from "./interfaces/ILiquidityLayerMessageRecipient.sol";
+
+contract ThemisRouter is Router, ILiquidityLayerRouter  {
     // remote.dispatch -> origin.handle -> auction.checkBid -> origin.dispatch -> remote.handle -> vault.refund
 
     // auction.distribute -> origin.dispatch -> remote.handle -> remote.dispatchWithTokens -> origin.handleWithTokens -> auction.mint
+    using TypeCasts for bytes32;
+    using SafeERC20 for IERC20;
 
     enum Action {
         DISPATCH,
+        LIQUIDITY,
         RESOLVE
     }
 
@@ -37,6 +47,17 @@ contract ThemisRouter is Router  {
         bytes data;
         bytes callback;
     }
+
+    struct LiquidityData {
+        bytes32 sender;
+        bytes32 recipient;
+        uint256 amount;
+        string bridge;
+        bytes adapterData;
+        bytes messageBody;
+    }
+
+    mapping(string => address) public liquidityLayerAdapters;
 
     function initialize(
         address _mailbox,
@@ -75,6 +96,54 @@ contract ThemisRouter is Router  {
         );
     }
 
+    function dispatchWithTokens(
+        uint32 _destinationDomain,
+        bytes32 _recipientAddress,
+        bytes calldata _messageBody,
+        address _token,
+        uint256 _amount,
+        string calldata _bridge
+    ) external payable returns (bytes32) {
+        ILiquidityLayerAdapter _adapter = _getAdapter(_bridge);
+
+        // transfer to the bridge adapter
+        IERC20(_token).safeTransferFrom(
+                msg.sender,
+                address(_adapter),
+                _amount
+        );
+
+        // Reverts if the bridge was unsuccessful.
+        // Gets adapter-specific data that is encoded into the message
+        // ultimately sent via Hyperlane.
+        bytes memory _adapterData = _adapter.sendTokens(
+            _destinationDomain,
+            _recipientAddress,
+            _token,
+            _amount
+        );
+
+        // The user's message "wrapped" with metadata required by this middleware
+        bytes memory _messageWithMetadata = abi.encode(
+            Action.LIQUIDITY,
+            LiquidityData({
+                sender: TypeCasts.addressToBytes32(msg.sender),
+                recipient: _recipientAddress, // The "user" recipient
+                amount: _amount, // The amount of the tokens sent over the bridge
+                bridge: _bridge, // The destination token bridge ID
+                adapterData: _adapterData, // The adapter-specific data
+                messageBody: _messageBody // The "user" message
+            })
+        );
+
+        // Dispatch the _messageWithMetadata to the destination's LiquidityLayerRouter.
+        return _dispatch(
+            _destinationDomain,
+            _messageWithMetadata
+        );
+    }
+
+
 
 
     function _handle(
@@ -82,12 +151,16 @@ contract ThemisRouter is Router  {
         bytes32 /* _sender */,
         bytes calldata _message
     ) internal override {
-        (Action action, address sender, Call memory call) = abi.decode(
-            _message,
-            (Action, address, Call)
-        );
+
+
+        Action action = abi.decode(_message, (Action));
 
         if (action == Action.DISPATCH) {
+
+            (, address sender, Call memory call) = abi.decode(
+                _message,
+                (Action, address, Call)
+            );
             (bool success, bytes memory result) = call.to.call(call.data);
 
             require(
@@ -113,6 +186,11 @@ contract ThemisRouter is Router  {
 
             emit HandledCall(sender, call.to, result);
         } else if (action == Action.RESOLVE) {
+            (, address sender, Call memory call) = abi.decode(
+                _message,
+                (Action, address, Call)
+            );
+
             (bool success, bytes memory result) = call.to.call(call.data);
 
             require(
@@ -121,6 +199,35 @@ contract ThemisRouter is Router  {
             );
 
             emit HandledCallback(sender, call.to, result);
+        } else if (action == Action.LIQUIDITY) {
+            (
+                , LiquidityData memory liqData
+            ) = abi.decode(
+                    _message,
+                    (Action, LiquidityData)
+            );
+
+            ILiquidityLayerMessageRecipient _userRecipient
+                = ILiquidityLayerMessageRecipient(
+                TypeCasts.bytes32ToAddress(liqData.recipient)
+            );
+
+            // Reverts if the adapter hasn't received the bridged tokens yet
+            (address _token, uint256 _receivedAmount) = _getAdapter(liqData.bridge)
+                .receiveTokens(
+                    _origin,
+                    address(_userRecipient),
+                    liqData.amount,
+                    liqData.adapterData
+            );
+
+            _userRecipient.handleWithTokens(
+                _origin,
+                liqData.sender,
+                liqData.messageBody,
+                _token,
+                _receivedAmount
+            );
         }
     }
 
@@ -135,5 +242,15 @@ contract ThemisRouter is Router  {
                 _to
             )
         );
+    }
+
+    function _getAdapter(string memory _bridge)
+        internal
+        view
+        returns (ILiquidityLayerAdapter _adapter)
+    {
+        _adapter = ILiquidityLayerAdapter(liquidityLayerAdapters[_bridge]);
+        // Require the adapter to have been set
+        require(address(_adapter) != address(0), "No adapter found for bridge");
     }
 }
---
+++ src/interfaces/ILiquidityLayerMessageRecipient.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.13;
+
+// author: @hyperlane-xyz
+interface ILiquidityLayerMessageRecipient {
+    function handleWithTokens(
+        uint32 _origin,
+        bytes32 _sender,
+        bytes calldata _message,
+        address _token,
+        uint256 _amount
+    ) external;
+}
